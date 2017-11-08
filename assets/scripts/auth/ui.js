c9f46e82287532f8bf35151e2d67ece719996be8
tacostacostacos <david.epstein@libertymutual.com>
2017-11-07 22:41:04 -0500

    Code Cleanup
    Changed how the show/hide/empty text functionality works in auth ui

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,6 +2,7 @@
 
 const store = require('../store.js')
 const sitesEvents = require('../sites/events.js')
+const reuse = require('../reuse/reuse.js')
 
 const signUpSuccess = function (data) {
   $('#messageContent').text('You have signed up as ' + data.user.email)
@@ -17,53 +18,61 @@ const signUpFailure = function () {
 
 const onSignInLinkClick = function (event) {
   event.preventDefault()
-  $('#sign-in').show()
-  $('#sign-up').hide()
-  $('#signInModal').hide()
-  $('#signUpModal').show()
-  $('#messageContent').hide()
-  $('#signInAfterSignUp').hide()
-  $('#signInMessage').show()
+  reuse.hideMultipleFields(['#sign-up', '#signInModal', '#messageContent', '#signInAfterSignUp'])
+  reuse.showMultipleFields(['#sign-in', '#signUpModal', '#signInMessage'])
+  // $('#sign-in').show()
+  // $('#sign-up').hide()
+  // $('#signInModal').hide()
+  // $('#signUpModal').show()
+  // $('#messageContent').hide()
+  // $('#signInAfterSignUp').hide()
+  // $('#signInMessage').show()
   const form = document.getElementById('sign-up')
   form.reset()
 }
 
 const onSignUpModalLinkClink = function (event) {
   event.preventDefault()
-  $('#sign-up').show()
-  $('#sign-in').hide()
-  $('#signUpModal').hide()
-  $('#signInModal').show()
-  $('#signInMessage').hide()
-  $('#messageContent').show()
-  $('#messageContent').text('')
-  $('#signUpSignIn').text('')
-  $('#signInMessage').text('')
-  $('#signInAfterSignUp').hide()
+  reuse.hideMultipleFields(['#sign-in', '#signUpModal', '#signInMessage', '#signInAfterSignUp'])
+  reuse.showMultipleFields(['#sign-up', '#signInModal', '#messageContent'])
+  reuse.emptyMultipleTextFields(['#messageContent', '#signUpSignIn', '#signInMessage'])
+  // $('#sign-up').show()
+  // $('#sign-in').hide()
+  // $('#signUpModal').hide()
+  // $('#signInModal').show()
+  // $('#signInMessage').hide()
+  // $('#messageContent').show()
+  // $('#messageContent').text('')
+  // $('#signUpSignIn').text('')
+  // $('#signInMessage').text('')
+  // $('#signInAfterSignUp').hide()
   const form = document.getElementById('sign-in')
   form.reset()
 }
 
 const signInSuccess = function (data) {
+  reuse.hideMultipleFields(['#sign-up', '#signInAfterSignUp', '#sign-in',
+    '#signUpLink', '#signUpModal', '#exampleModal', '#get-sites', '#mySite',
+    '#myPages', '#myBlogs', '#mySiteHeader'])
+  reuse.showMultipleFields(['#signInMessage', '#signOut', '#changePassword', '#userNameNav'])
   $('#messageContent').text('')
-  $('#sign-up').hide()
-  $('#signInAfterSignUp').hide()
-  $('#signInMessage').show()
+  // $('#sign-up').hide()
+  // $('#signInAfterSignUp').hide()
+  // $('#signInMessage').show()
   $('#signInMessage').text('Signed in as ' + data.user.email)
-  $('#sign-in').hide()
-  $('#signUpLink').hide()
-  $('#signOut').show()
-  $('#changePassword').show()
-  $('#signUpModal').hide()
-  $('#exampleModal').modal('hide')
-  $('#userNameNav').show()
-  $('#get-sites').hide()
-  $('#mySite').hide()
-  $('#myPages').hide()
-  $('#myBlogs').hide()
-  $('#mySiteHeader').hide()
+  // $('#sign-in').hide()
+  // $('#signUpLink').hide()
+  // $('#signOut').show()
+  // $('#changePassword').show()
+  // $('#signUpModal').hide()
+  // $('#exampleModal').modal('hide')
+  // $('#userNameNav').show()
+  // $('#get-sites').hide()
+  // $('#mySite').hide()
+  // $('#myPages').hide()
+  // $('#myBlogs').hide()
+  // $('#mySiteHeader').hide()
   store.user = data.user
-  console.log('store.user is ', store.user)
   const user = store.user.email
   $('#userNameNav').append(user)
   sitesEvents.getUpdatedSiteByUser()
@@ -74,26 +83,35 @@ const signInFailure = function () {
 }
 
 const signInNewUserSuccess = function (data) {
+  reuse.hideMultipleFields(['#sign-up', '#signInAfterSignUp', '#sign-in',
+    '#signUpLink', '#signUpModal', '#signInMessage', '#userSignedOut', '#userSignedOut'])
+  reuse.showMultipleFields(['#signOut', '#changePassword', '#userNameNav', '#signedIn', '#createASite'])
   $('#messageContent').text('')
-  $('#sign-up').hide()
-  $('#signInAfterSignUp').hide()
-  $('#sign-in').hide()
-  $('#signUpLink').hide()
-  $('#signOut').show()
-  $('#changePassword').show()
-  $('#signUpModal').hide()
-  $('#userNameNav').show()
-  $('#signInMessage').hide()
+  // $('#sign-up').hide()
+  // $('#signInAfterSignUp').hide()
+  // $('#sign-in').hide()
+  // $('#signUpLink').hide()
+  // $('#signOut').show()
+  // $('#changePassword').show()
+  // $('#signUpModal').hide()
+  // $('#userNameNav').show()
+  // $('#signInMessage').hide()
   store.user = data.user
   const user = store.user.email
   $('#userNameNav').append(user)
   $('#exampleModal').modal('hide')
-  $('#userSignedOut').hide()
-  $('#signedIn').show()
-  $('#createASite').show()
+  // $('#userSignedOut').hide()
+  // $('#signedIn').show()
+  // $('#createASite').show()
 }
 
 const signOutSuccess = function () {
+  reuse.hideMultipleFields(['#changePassword', '#signOut', '#signUpModal', '#signedIn'])
+  reuse.showMultipleFields(['#signUpLink', '#messageContent', '#signInMessage', '#signInModal',
+    '#get-sites', '#userSignedOut'])
+  reuse.emptyMultipleTextFields(['#messageContent', '#signInMessage', '#userNameNav',
+    '#blogs', '#allSites', '#sitePages'])
+
   store.user = null
   store.site = null
   $('#changePassword').hide()
@@ -108,12 +126,12 @@ const signOutSuccess = function () {
   $('#get-sites').show()
   $('#signedIn').hide()
   $('#userSignedOut').show()
-  $('#userNameNav').text('')
+  // $('#userNameNav').text('')
   $('#siteTitle').text('SSERPDROW')
   $('#siteDescription').text('Welcome to SSERPDROW! To view our communities\' sites please click \'View All Sites\' link above. To create your own site/blogs please sign-up or sign-in!')
-  $('#blogs').text('')
-  $('#allSites').text('')
-  $('#sitePages').text('')
+  // $('#blogs').text('')
+  // $('#allSites').text('')
+  // $('#sitePages').text('')
   $('#editASite').hide()
   $('#manageBlogSection').hide()
   $('#newBlog').hide()
